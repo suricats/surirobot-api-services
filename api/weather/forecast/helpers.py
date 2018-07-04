b180c627bfc16e8e1690f31981b0322a934712fb
Alain BERRIER <alain.berrier@outlook.com>
2018-07-04 22:10:35 +0200

    weather module

--- api/weather/forecast/helpers.py
+++ api/weather/forecast/helpers.py
@@ -8,12 +8,15 @@ with open(os.getcwd() + '/res/credentials/forecast.json', 'r') as file:
     FORECAST_CREDENTIALS = json.load(file)
 token = FORECAST_CREDENTIALS['API_KEY']
 
-def forecast_get_weather(source='google-news-fr'):
-    res = requests.get('https://api.darksky.net/forecast/' + token + '/37.8267,-122.4233')
+def forecast_get_weather(lat, long, time, language):
+    params = {'lang': language, 'units': 'auto'}
+    res = requests.get('https://api.darksky.net/forecast/' + token + '/' + str(lat) + ',' + str(long) + ',' + str(time), params=params)
     print(res.content)
     if res.status_code == 200:
         return res.json()
     elif res.status_code == 400:
-        raise BadParameterException(source)
+        raise ExternalAPIException(api_name='Dark Sky', description=res.json())
+    elif res.status_code == 401:
+        raise InvalidCredentialsException('Dark Sky')
     else:
-        raise ExternalAPIException(api_name='Google News', description='http_code:{}'.format(res.status_code))
+        raise ExternalAPIException(api_name='Dark Sky', description='http_code:{}'.format(res.status_code))
--- api/weather/forecast/views.py
+++ api/weather/forecast/views.py
@@ -4,11 +4,56 @@ from flask import Blueprint, request, jsonify, Response
 from api.exceptions import MissingParameterException, InvalidCredentialsException, \
     BadParameterException, ExternalAPIException, APIException, MissingHeaderException, BadHeaderException, OperationFailedException
 
+from .helpers import forecast_get_weather
+
 weather_forecast = Blueprint('weather_forecast', __name__)
 logger = logging.getLogger(__name__)
 
 
-@weather_forecast.route('/text', methods=['POST'], defaults={'want': 'text'})
-@weather_forecast.route('/audio', methods=['POST'],  defaults={'want': 'audio'})
-def weather(want):
+@weather_forecast.route('/', methods=['POST'])
+def weather():
     errors = []
+
+    if request.json:
+        if 'latitude' not in request.json:
+            errors.append(dict(MissingParameterException('latitude')))
+
+        if 'longitude' not in request.json:
+            errors.append(dict(MissingParameterException('longitude')))
+
+        if 'time' not in request.json:
+            errors.append(dict(MissingParameterException('time')))
+
+        if 'language' not in request.json:
+            errors.append(dict(MissingParameterException('language')))
+
+        if errors:
+            return jsonify({'errors': errors}), 400
+
+        latitude = request.json['latitude']
+        longitude = request.json['longitude']
+        time = request.json['time']
+        language = request.json['language']
+        try:
+            res = forecast_get_weather(latitude, longitude, time, language)
+            output = {
+                "apparentTemperature": res["currently"]["apparentTemperature"],
+                "cloudCover": res["currently"]["cloudCover"],
+                "humidity": res["currently"]["humidity"],
+                "precipIntensity": res["currently"]["precipIntensity"],
+                "precipProbability": res["currently"]["precipProbability"],
+                "summary": res["currently"]["summary"],
+                "temperature": res["currently"]["temperature"]
+            }
+            return jsonify(output), 200
+        except InvalidCredentialsException as e:
+            return jsonify({'errors': [dict(e)]}), e.status_code
+        except ExternalAPIException as e:
+            return jsonify({'errors': [dict(e)]}), e.status_code
+        except Exception as e:
+            logger.error(e)
+            return jsonify({'errors': [dict(APIException('weather_parse({})'.format(type(e).__name__)))]}), 500
+
+    else:
+        errors.append(dict(APIException('no_content')))
+        return jsonify({'errors': errors}), 400
